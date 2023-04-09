3d43748a804f0aea01088c19a888861b367281db
tmuzzillo <tomimuzzillo@gmail.com>
2023-04-08 21:33:39 -0300

    adding discovery service and spring cloud environment with eureka

--- order-service/src/main/java/com/microservicecourse/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/microservicecourse/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -39,9 +39,9 @@ public class OrderService {
                 .toList();
 
         // Call inventory service, and place order if product is in stock.
-       InventoryResponse[] inventoryResponseArray = webClient.get()
+       InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
                //Build the URI getting the skucodes and putting into request parameters
-                .uri("http://localhost:8082/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .uri("http://inventory-service/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                         .retrieve()
                                 .bodyToMono(InventoryResponse[].class)
                                         .block();
